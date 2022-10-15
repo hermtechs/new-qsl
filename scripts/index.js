588467f9804df1caba5c2f95aa83dbcc0362188c
gafayo <gafayoabel20@gmail.com>
2022-10-15 23:05:38 +0300

    progress bar and scroll to top

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,33 @@
 const navBtn = document.querySelector('.humburger');
+//! SCROLL-TOP NAV STARTS FROM HERE
+const navbar = document.querySelector("Nav")
+let lastScroll = 0;
+window.addEventListener("scroll", function(){
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScroll){
+        navbar.style.top="-80px"
+    }else{
+        navbar.style.top="0";
+    }
+
+    lastScroll = scrollTop;
+
+})
+//! SCROLL-TOP NAV ENDS FROM HERE
+
+// !PROGRESS BAR & Back-TO-Top STARTS FROM HERE
+const progressBar = document.getElementById("Progress-bar")
+const BackTop = document.querySelector(".Back-to-top")
+console.log(progressBar)
+// percentage Calculation;
+document.addEventListener("scroll", updateWidth)
+
+// updateWidth function
+function updateWidth(){
+    // console.dir(document.documentElement)
+    const {scrollTop, scrollHeight} = document.documentElement;
+    const widthPercent = `${(scrollTop /(scrollHeight - window.innerHeight)) * 100}%`
+    // console.log(widthPercent)
+    progressBar.style.setProperty("--progress", widthPercent)
+
+}
